f8da7f9fefeb026820a7d09f7fb74fa7afddccec
Renz5678 <lawrenzmatthewpgarcia@iskolarngbayan.pup.edu.ph>
2026-06-25 15:47:30 +0800

    Made users models and test, as well as babel and jest configs

---
+++ server/src/models/users.js
@@ -0,0 +1,51 @@
+'use strict';
+import { Model, Sequelize } from 'sequelize';
+import bcrypt from 'bcrypt';
+
+export default (sequelize, DataTypes) => {
+    class Users extends Model { };
+
+    Users.init({
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: { msg: 'Username must not be null' },
+                notEmpty: { msg: 'Username must not be empty' }
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: { msg: 'Email must not be empty' },
+                notNull: { msg: 'Email must not be null' },
+                isEmail: { msg: 'User must have a valid email' }
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: { msg: 'Password must not be empty' },
+                notNull: { msg: 'Password must not be null' }
+            }
+        }
+    },
+        {
+            sequelize,
+            modelName: 'Users'
+        });
+
+    Users.beforeCreate(async (user) => {
+        user.password = await bcrypt.hash(user.password, 10);
+    })
+
+    Users.comparePassword = async (password, user) => {
+        return await bcrypt.compare(password, user.password);
+    }
+
+    Users.associate = (models) => { }
+
+    return Users;
+}
---
+++ server/src/tests/users.test.js
@@ -0,0 +1,57 @@
+import { Sequelize, DataTypes } from "sequelize";
+import UsersFactory from '../models/users.js'
+
+const sequelize = new Sequelize('sqlite::memory:', { logging: false });
+const Users = UsersFactory(sequelize, DataTypes);
+
+describe('Users model', () => {
+    it('should hash password before create', async () => {
+        const user = Users.build({
+            username: 'test',
+            email: 'test@gmail.com',
+            password: 'plainpassword'
+        });
+
+        await Users.runHooks('beforeCreate', user, {});
+
+        expect(user.password).not.toBe('plainpassword');
+        expect(user.password.length).toBeGreaterThan(0);
+    });
+
+    it('should compare password correctly', async () => {
+        const user = Users.build({
+            username: 'test',
+            email: 'test@gmail.com',
+            password: 'plainpassword'
+        });
+
+        await Users.runHooks('beforeCreate', user, {});
+
+        const match = await Users.comparePassword('plainpassword', user);
+        const mismatch = await Users.comparePassword('wrongpassword', user);
+
+        expect(match).toBe(true);
+        expect(mismatch).toBe(false);
+    });
+
+
+    it('should fail validation on empty username', async () => {
+        const user = Users.build({
+            username: '',
+            email: 'test@gmail.com',
+            password: 'plainpassword'
+        })
+
+        await expect(user.validate()).rejects.toThrow();
+    });
+
+    it('should fail validation on wrong email', async () => {
+        const user = Users.build({
+            username: 'renz',
+            email: 'testgmail.com',
+            password: 'plainpassword'
+        })
+
+        await expect(user.validate()).rejects.toThrow();
+    });
+});
