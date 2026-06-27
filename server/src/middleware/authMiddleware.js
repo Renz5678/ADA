94c207607e9ade69f92895db52c820b221937ee4
Renz5678 <lawrenzmatthewpgarcia@iskolarngbayan.pup.edu.ph>
2026-06-27 14:10:10 +0800

    Added auth middleware

---
+++ server/src/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const authMiddleware = async (req, res, next) => {
+    const auth = req.headers['authorization'];
+
+    if (!auth || !auth.startsWith('Bearer ')) return res.status(401).json({ message: 'Unauthorized!' });
+
+    const token = auth.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid or expired token!' });
+    }
+}
+
+export default authMiddleware;
