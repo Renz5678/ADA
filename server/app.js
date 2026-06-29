259458b4240a3c2c204a220eb494839b34297a61
Renz5678 <lawrenzmatthewpgarcia@iskolarngbayan.pup.edu.ph>
2026-06-29 13:44:10 +0800

    Configured CORS for frontend

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors'
+
 import authRouter from './src/routes/auth.js';
 import productRouter from './src/routes/product.js';
 import orderRouter from './src/routes/order.js';
@@ -9,6 +11,12 @@ import orderItemRouter from './src/routes/orderItem.js';
 import { authLimiter, generalLimiter } from './src/middleware/rateLimiter.js'
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
