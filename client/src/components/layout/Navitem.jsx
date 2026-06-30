08b9eec3cdf8a52f811bdbf388a07cfd1205c6ac
Renz5678 <lawrenzmatthewpgarcia@iskolarngbayan.pup.edu.ph>
2026-06-30 20:10:53 +0800

    Added dashboard design

--- client/src/components/layout/Navitem.jsx
+++ client/src/components/layout/Navitem.jsx
@@ -1,7 +1,16 @@
-export default function Navitem({ navName }) {
+import { useState } from "react";
+
+export default function Navitem({ navName, isActive, onClick }) {
     return (
-        <div className="w-[100%] h-14 bg-white flex items-center p-4">
+        <div
+            onClick={onClick}
+            className={`w-full h-14 transition-all items-center p-4 cursor-pointer font-body
+                ${isActive
+                    ? "bg-[#FFB2B9] text-[#71333C] scale-95 rounded-r-4xl border-l-6 border-[#71333C]"
+                    : "text-[#E8F2FF] hover:bg-[#FFB2B9] hover:text-[#71333C] hover:scale-95 "
+                }`}
+        >
             {navName}
         </div>
-    )
+    );
 }
--- client/src/components/layout/Sidebar.jsx
+++ client/src/components/layout/Sidebar.jsx
@@ -1,12 +1,12 @@
-const navItems = [
-    'Dashboard',
-    'Orders',
-    'Products',
-    'Inventory'
-]
 import Navitem from "./Navitem"
+import { useState } from "react";
+import { MdDashboard } from "react-icons/md";
+
+const navItems = ["Dashboard", "Orders", "Products", "Expenses"];
 
 export default function Sidebar() {
+    const [activeNav, setActiveNav] = useState("Dashboard");
+
     return (
         <div className="flex flex-none flex-col w-[20%] h-full bg-[#0F1D29] p-4">
             <div className="font-headline text-white font-semibold text-4xl text-left p-2">
@@ -16,7 +16,16 @@ export default function Sidebar() {
                 Create. Sell. Track
             </div>
 
-            <Navitem navName={'Dashboard'} />
+            <div className="flex flex-col gap-2 mt-8">
+                {navItems.map((name) => (
+                    <Navitem
+                        key={name}
+                        navName={name}
+                        isActive={activeNav === name}
+                        onClick={() => setActiveNav(name)}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
