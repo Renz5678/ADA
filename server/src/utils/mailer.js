7bb3a61d0b6a217cbd729119743cb184320a0af9
Scarecrow <lawrenzmatthewpgarcia@iskolarngbayan.pup.edu.ph>
2026-07-07 21:45:58 +0800

    fixed auth test

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -3,12 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
-});
+let transporter;
+
+if (process.env.NODE_ENV === 'test') {
+    transporter = {
+        sendMail: async () => {
+            // Mock email sending for tests
+            return true;
+        }
+    };
+} else {
+    transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+}
 
 export default transporter;
