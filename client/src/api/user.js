67676dfe5e8d47d46b54b2cd94ca8ddf6ee0a856
Scarecrow <lawrenzmatthewpgarcia@iskolarngbayan.pup.edu.ph>
2026-07-15 15:43:22 +0800

    fix: remove explicit Content-Type headers for profile image upload

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -5,6 +5,4 @@ export const updateBusinessName = (data) => api.patch('/user-details/business-na
 
 export const getProfile = () => api.get('/profile').then(r => r.data);
 export const updateProfile = (data) => api.put('/profile', data).then(r => r.data);
-export const uploadProfileImages = (formData) => api.post('/profile/upload', formData, {
-    headers: { 'Content-Type': 'multipart/form-data' }
-}).then(r => r.data);
+export const uploadProfileImages = (formData) => api.post('/profile/upload', formData).then(r => r.data);
