d606934e58d6cfcf329d0986b85b14d428245508
Scarecrow <lawrenzmatthewpgarcia@iskolarngbayan.pup.edu.ph>
2026-07-16 21:26:36 +0800

    chore: remove seed and test scripts

--- scratch-test-db.js
+++
@@ -1,7 +0,0 @@
-import { models, sequelize } from "./server/src/models/index.js";
-async function test() {
-  const products = await models.Product.findAll({ attributes: ['product_id', 'product_name', 'image_url'] });
-  console.log(JSON.stringify(products, null, 2));
-  process.exit(0);
-}
-test();
