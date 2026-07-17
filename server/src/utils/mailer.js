2d21f916cbc5f99d1e2bb2299df52392f1a3689a
Scarecrow <lawrenzmatthewpgarcia@iskolarngbayan.pup.edu.ph>
2026-07-17 20:56:23 +0800

    fix: add timeouts to nodemailer to prevent hanging

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -21,7 +21,10 @@ if (process.env.NODE_ENV === 'test') {
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
-        }
+        },
+        connectionTimeout: 10000,
+        greetingTimeout: 10000,
+        socketTimeout: 10000
     });
 }
 
