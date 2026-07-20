64bca1724cd6ca2e3b5988bf232e2c812cc0c76d
Scarecrow <lawrenzmatthewpgarcia@iskolarngbayan.pup.edu.ph>
2026-07-20 16:55:58 +0800

    chore: move client URLs to env vars, remove hardcoded Vercel URL

--- server/app.js
+++ server/app.js
@@ -43,7 +43,6 @@ const allowedOrigins = [
     'http://127.0.0.1:5173',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://ada-pied-iota.vercel.app'
 ];
 if (process.env.CLIENT_URL) {
     allowedOrigins.push(process.env.CLIENT_URL);
