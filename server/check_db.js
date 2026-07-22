9ff1ffa3c10dc9e9f2e2b4cc40347ef9fde7b4cf
Scarecrow <lawrenzmatthewpgarcia@iskolarngbayan.pup.edu.ph>
2026-07-22 15:15:09 +0800

    chore(scripts): move debug scripts to debug_scripts folder, update ignore files, and remove hardcoded force_admin_digest password

--- server/check_db.js
+++
@@ -1,7 +0,0 @@
-import { models } from './src/models/index.js';
-async function main() {
-  const users = await models.Users.findAll({ attributes: ['user_id', 'profile_picture'] });
-  console.log(users.map(u => u.toJSON()));
-  process.exit(0);
-}
-main();
--- server/get_user.js
+++
@@ -1,8 +0,0 @@
-import { sequelize } from './src/models/index.js';
-
-async function run() {
-  const [results] = await sequelize.query('SELECT * FROM "Users" LIMIT 1');
-  console.log(results);
-  process.exit(0);
-}
-run();
